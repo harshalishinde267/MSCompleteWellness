25636e83b5945ad871bb14bb69b51f2bba155d85
harshalishinde267 <109190708+harshalishinde267@users.noreply.github.com>
2026-02-18 23:43:26 +0530

    Add files via upload

---
+++ Services.js
@@ -0,0 +1,18 @@
+// import DermatologyVideos from "../components/DermatologyVideos";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import OurServices from "../components/OurServices"
+
+
+function Services() {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <OurServices />
+            <Footer />
+        </>
+    );
+}
+export default Services;
